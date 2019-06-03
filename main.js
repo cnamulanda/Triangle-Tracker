b90d3d36d70e65a251bdbe7cb4e89ab9e9dc88f5
Christine <christine.namulanda@gmail.com>
2019-06-03 04:03:48 -0400

    format code

---
+++ main.js
@@ -0,0 +1,19 @@
+unction calculate(){
+    var Trianglelength=parseFloat(document.getElementById("l").value);
+    var TRianglewidth=parseFloat(document.getElementById("w").value);
+    var TRianglebase=parseFloat(document.getElementById("b").value);
+    
+    if((Trianglelength+Trianglewidth)<=Trianglebase || (Trianglewidth+Trianglebase)<=Trianglelength||(Trianglebase+Trianglelength)<=Trianglewidth){
+    document.getElementById('answer').innerHTML="Not a triangle"
+    }else if((Trianglelength == Trianglewidth) && (TRianglewidth == Trianglebase) && (Trianglebase == Trianglelength)){
+     document.getElementById('answer') .innerHTML="Equilateral"
+    }else if((Trianglelength == TRianglewidth)||(Trianglewidth == Trianglebase)||(Trianglebase == Trianglelength)){
+    document.getElementById('answer') .innerHTML="isoceles"
+    }else if ((Trianglelength!==TRianglewidth) && (Trianglewidth!==Trianglebase)&&(Trianglebase!==Trianglelength)){
+     document.getElementById('answer').innerHTML="scalene"
+    }
+    };
+    
+    function change(){
+     document.getElementByClassName('reset').innerHTML="";
+    }
